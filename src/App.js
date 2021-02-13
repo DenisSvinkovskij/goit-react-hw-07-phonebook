d0040a37f4cf0b04fcf0887a8a7634ba0208fbe3
DenisSvinkovskij <1911.denis55@gmail.com>
2021-02-13 17:41:42 +0200

    done hw-07

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import phonebookSelectors from './redux/phonebook/phonebook-selectors';
+import phonebookOperations from './redux/phonebook/phonebook-operations';
+import ContactForm from './components/ContactForm/ContactForm';
+import Filter from './components/Filter/Filter';
+import ContactList from './components/ContactList/ContactList';
+import { CSSTransition } from 'react-transition-group';
+import Loader from './components/Loader/Loader';
+import './App.css';
+
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchContacts();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.props.isLoading && <Loader />}
+
+        <div className="Container">
+          <CSSTransition
+            in={true}
+            appear={true}
+            timeout={500}
+            classNames="title"
+            unmountOnExit
+          >
+            <h1>Phonebook</h1>
+          </CSSTransition>
+
+          <ContactForm />
+
+          <h1>Contacts</h1>
+
+          <Filter />
+
+          <ContactList />
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  isLoading: phonebookSelectors.getLoading(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchContacts: () => dispatch(phonebookOperations.fetchContacts()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
